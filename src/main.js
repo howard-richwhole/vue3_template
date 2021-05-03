4cebdb14c1c9c131f03f9b6dd5ffc973d90ca2cf
howard-richwhole <79359287+howard-richwhole@users.noreply.github.com>
2021-05-03 14:29:27 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import globalComp from './components'
 import globalUtils from './utils'
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
-import store from './store'
 import './styles/index.sass'
+import App from './App.vue'
+import store from './store'
 
 createApp(App)
   .use(store)
