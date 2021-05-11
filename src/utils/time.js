856cc8b733e36ac2935bede7eaacd4372db42626
howard-richwhole <79359287+howard-richwhole@users.noreply.github.com>
2021-05-11 13:27:31 +0800

    Update time.js

--- src/utils/time.js
+++ src/utils/time.js
@@ -6,8 +6,7 @@ import moment from 'moment/min/moment.min'
  * @param {{formatOutput:?string,formatInput: ?string}} opts
  * @return {string}
  */
-const timeFormat = (time, opts) => {
-  if (!opts) opts = {}
+const timeFormat = (time, opts = {}) => {
   // https://momentjs.com/docs/#/displaying/format/
   // 年YYYY、月:MM(01)M(1)、日:DD(01)D(1)、時HH(01)H(1)、分mm(01)m(1)、秒ss(01)s(1)
   return moment(time, opts.formatInput).format(opts.formatOutput)
@@ -15,15 +14,16 @@ const timeFormat = (time, opts) => {
 
 /**
  * 加五天 timeAdd('2000-01-01',5,''d) === 2000-01-06
- *  {(String|Date|Number|moment)} time
- *  {Number} dur 間格時間
+ * @param {Moment | string | Date | number} time
+ * @param {Number} dur 間格時間
+ * @param {?unit:string,?endUnit:string,?formatInput:string,?formatOutput:string} opts
  *  {String} unit 單位
  *  {String} endUnit 結尾時間單位，不填則不使用； 'd' -> 2000-01-06 23:59:59
  *  {String} formatInput 輸入時間格式
  *  {String} formatOutput 輸出時間格式
+ * @returns {string}
  */
-const timeAdd = (time, dur, opts) => {
-  if (!opts) opts = {}
+const timeAdd = (time, dur, opts = {}) => {
   // https://momentjs.com/docs/#/manipulating/add/
   // years y、quarters Q、months M、weeks w、days d、hours h、minutes m、seconds s
   const momentTime = moment(time, opts.formatInput)
@@ -35,15 +35,16 @@ const timeAdd = (time, dur, opts) => {
 }
 
 /**
- * 時間間距
- *  {String} time1
- *  {String} time2
+ *
+ * @param {string} time1 目標時間1
+ * @param {string} time2 目標時間2
+ * @param {?floatResult:boolean,?unit:string,:formatInput:string} opts
  *  {Boolean} floatResult 是否有小數點
  *  {String} unit 單位
  *  {String} formatInput 時間輸入格式
+ * @returns number
  */
-const timeDiff = (time1, time2, opts) => {
-  if (!opts) opts = {}
+const timeDiff = (time1, time2, opts = {}) => {
   if (!opts.unit) opts.unit = 'd'
   const time1_moment = moment(time1, opts.formatInput)
   const time2_moment = moment(time2, opts.formatInput)
